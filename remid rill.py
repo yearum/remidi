c0f4f7fec30df37619156af996ae13ccf61b2c33
yaeum <jongyerum@gmail.com>
2024-11-06 09:26:56 +0700

    simpan semua perubahan

---
+++ remid rill.py
@@ -0,0 +1,21 @@
+pilihan = input("masukan pilihan(A/B):")
+
+if pilihan == 'A':
+    print("red+green =yellow")
+    print("red+blue =magenta")
+    print("blue+green =cyan")
+    print("red+green+blue =whit")
+
+if pilihan == 'B':
+    warna1 = str("masukan warna1:")
+    warna2 = str("masukan warna2:")
+    
+if warna1 ("red end warna2"):
+    print("warna baru")
+elif warna1 ("blue end warna2"):
+    print("warna baru")
+elif warna1 ("green end warna2"):
+    print("warna baru")
+elif warna1 ("cyan end warna2"):
+    print("warna baru")
+
---
+++ remidi riil2.py
@@ -0,0 +1,11 @@
+bilangan1 = input("masukan bilangan 1:")
+bilangan2 = input("masukan bilangan 2:")
+bilangan3 = input("masukan bilangan 3:")
+
+if bilangan1 > bilangan2 and bilangan2 > bilangan3:
+    print("bilangan urut naik")
+if bilangan1 < bilangan2:
+    print("bilangan urut turun")
+else:
+    bilangan1 == bilangan2 
+    print("bilangan acak")
